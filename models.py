ae32e1055d063b926c8ea01872e93b0d448911f3
rohitm2710 <rohitmajumder05@gmail.com>
2026-09-11 17:10:04 +0530

    mcq feature added

--- models.py
+++ models.py
@@ -1,5 +1,14 @@
 from pydantic import BaseModel
 
+class mcq(BaseModel):
+    difficulty: str
+    question: str
+    option_a: str
+    option_b: str
+    option_c: str
+    option_d: str
+    answer: str
+
 class Question(BaseModel):
     category: str
     difficulty: str
